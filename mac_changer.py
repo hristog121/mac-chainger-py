5257096ec9724d75b25c0a25ac3368bd7656be1d
Hristo Georiev <hristog@kth.se>
2020-03-17 09:31:50 -0400

    Introducing variables for interface and value for the new MAC address

--- mac_changer.py
+++ mac_changer.py
@@ -2,7 +2,13 @@
 
 import subprocess
 
+# Variables for interface and value for the new mac
+interfece = "eth0"
+new_mac = "00:11:22:33:44:77"
+
+print("[+] Changing the MAC address for " + interfece + " to " + new_mac)
+
 # Run the ifconfig as a shell command
-subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44", shell=True)
-subprocess.call("ifconfig eth0 up", shell=True)
+subprocess.call("ifconfig " + interfece + " down", shell=True)
+subprocess.call("ifconfig " + interfece + " hw ether" + new_mac, shell=True)
+subprocess.call("ifconfig " + interfece + " up", shell=True)
