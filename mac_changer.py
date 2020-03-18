4700677957602495526f478a3b37ab71cb40f6eb
Hristo Georiev <hristog@kth.se>
2020-03-18 07:58:03 -0400

    User can choose interface, and value for MAC

--- mac_changer.py
+++ mac_changer.py
@@ -3,12 +3,12 @@
 import subprocess
 
 # Variables for interface and value for the new mac
-interfece = "eth0"
-new_mac = "00:11:22:33:44:77"
+interface = input("Interface > ")
+new_mac = input("New MAC > ")
 
-print("[+] Changing the MAC address for " + interfece + " to " + new_mac)
+print("[+] Changing the MAC address for " + interface + " to " + new_mac)
 
 # Run the ifconfig as a shell command
-subprocess.call("ifconfig " + interfece + " down", shell=True)
-subprocess.call("ifconfig " + interfece + " hw ether" + new_mac, shell=True)
-subprocess.call("ifconfig " + interfece + " up", shell=True)
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
