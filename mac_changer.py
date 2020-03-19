9fb1066f708f5cef534beb1a5f4f59b7f9202dfe
Hristo Georiev <hristog@kth.se>
2020-03-19 12:29:40 -0400

    Split the into functions

--- mac_changer.py
+++ mac_changer.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
 
-# Variables for interface and value for the new mac
-interface = input("Interface > ")
-new_mac = input("New MAC > ")
 
-print("[+] Changing the MAC address for " + interface + " to " + new_mac)
+def get_arguments():
+    # Take arguments from the user directly on the command line
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
 
-# Run the ifconfig as a shell command
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
+    return parser.parse_args()
+
+
+def change_mac(interface, new_mac):
+    print("[+] Changing the MAC address for " + interface + " to " + new_mac)
+
+    # Run the ifconfig as a shell command  - no protection against command injection
+    # subprocess.call("ifconfig " + interface + " down", shell=True)
+    # subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+    # subprocess.call("ifconfig " + interface + " up", shell=True)
+
+    # Same commands as above but command injection will bnot be possible
+    subprocess.call(["ifconfig ", interface, "down"])
+    subprocess.call(["ifconfig ", interface, "hw ether ", new_mac])
+    subprocess.call(["ifconfig ", interface, "up"])
+
+
+(options, arguments) = get_arguments()
+change_mac(options.interface, options.new_mac)
